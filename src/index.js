74eeb1e41e1f0222dcde2b235bde8ca3f073665b
Chris Hannaby <chris.hannaby@netlify.com>
2022-05-17 17:00:22 -0600

    use execSync

--- src/index.js
+++ src/index.js
@@ -1,35 +1,34 @@
-import utils from "util";
-import { exec } from "child_process";
-import fs from "fs";
-import AWS from "aws-sdk";
-
-function execute(command) {
-  return utils.promisify(exec)(command);
-}
+import utils from 'util'
+import { execSync } from 'child_process'
+import fs from 'fs'
+import AWS from 'aws-sdk'
 
 async function uploadToS3(fileName) {
+  console.log(`Uploading ${fileName} to S3`)
   const s3 = new AWS.S3({
     accessKeyId: process.env.MY_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.MY_AWS_SECRET_ACCESS_KEY,
-    region: "us-east-1"
-  });
-  console.log(process.cwd());
-  const fileContent = fs.readFileSync(fileName);
+    region: 'us-east-1',
+  })
+  console.log(process.cwd())
+  const fileContent = fs.readFileSync(fileName)
   const params = {
     Bucket: process.env.MY_AWS_BUCKET_NAME,
     Key: fileName,
     Body: fileContent,
-  };
+  }
   s3.putObject(params, (err, data) => {
     if (err) {
-      console.error(err);
+      console.error(err)
     }
-    console.log(`${fileName} uploaded to ${data.Location}`);
-  });
+    console.log(`${fileName} uploaded to ${data.Location}`)
+  })
 }
 
 export const onSuccess = async function ({ constants: { PUBLISH_DIR } }) {
-  const tarName = `${process.env.COMMIT_REF}.tgz`;
-  await execute(`tar czf ${tarName} ${PUBLISH_DIR}`);
-  uploadToS3(tarName);
-};
+  const tarName = `${process.env.COMMIT_REF}.tgz`
+  execSync(`tar vczf ${tarName} ${PUBLISH_DIR}`, {
+    stdio: 'inherit',
+  })
+  uploadToS3(tarName)
+}
